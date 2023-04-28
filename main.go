081e017d724d3e15acba2eacc749378d2a168266
Wydy <1937986+wydy@users.noreply.github.com>
2023-04-28 22:43:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -212,7 +212,7 @@ func release(source string, destination string, output string) error {
 }
 
 func main() {
-	err := release("loyalsoldier/domain-list-custom", "wydy/sing-geosite", "geosite.db")
+	err := release("loyalsoldier/v2ray-rules-dat", "wydy/sing-geosite", "geosite.db")
 	if err != nil {
 		logrus.Fatal(err)
 	}
